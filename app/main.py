a29e30317d1a29cdf4ae7ca4fb33a53a52ca56be
Roberto Angel Melendez-Armenta <103074095+angelarmenta@users.noreply.github.com>
2025-09-22 17:13:30 -0600

    [Add] Agregué la función para mostrar un tablero de las tareas por estatus

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from utils.utils import get_task, save_tasks, Task
+from utils.utils import get_task, save_tasks, Task, TaskStatus
 
 if "tasks" not in st.session_state:
     st.session_state["tasks"] = get_task()
@@ -26,6 +26,35 @@ def show_add_form():
     #st.write(title)
     #st.write(description)
 
+def show_tasks_by_status(status: TaskStatus):
+    tasks = [task for task in st.session_state.tasks if task.get("status","") == status.value]
+
+    if status == TaskStatus.PENDING:
+        st.warning("Pending", icon="⚠️")
+    elif status == TaskStatus.IN_PROGRESS:
+        st.warning("In progress", icon="📌")
+    else:
+        st.warning("Completed", icon="✅")
+        
+    for task in tasks:
+        task_id = task.get("id","")
+
+        with st.expander(task.get("title",""), expanded=False):
+            st.markdown(f":green[{task.get('timestamp','')}]")
+            st.write(task.get("description",""))
+
+def show_tasks():
+    col_pending, col_in_progress, col_completed = st.columns(3)
+
+    with col_pending:
+        show_tasks_by_status(TaskStatus.PENDING)
+    
+    with col_in_progress:
+        show_tasks_by_status(TaskStatus.IN_PROGRESS)
+
+    with col_completed:
+        show_tasks_by_status(TaskStatus.COMPLETED)
+
 def main():
     st.header("Mi app del Curso GitHub")
 
@@ -35,6 +64,7 @@ def main():
     
     #Mostrar el formulario
     show_add_form()
+    show_tasks()
 
 if __name__ == "__main__":
     main()
